db38516bbeb686acbdd52afc3cff49aaaa8d0ee9
nobuti <buti@nobuti.com>
2020-05-03 09:23:39 +0200

    Colors process

---
+++ main.js
@@ -0,0 +1,82 @@
+require('dotenv').config()
+
+const express = require('express')
+const fetch = require('isomorphic-fetch')
+const figma = require('./lib/figma')
+
+const canvasMap = {}
+
+const app = express()
+
+function preprocessTree (canvas, node) {
+  if (!canvasMap[canvas.id]) {
+    canvasMap[canvas.id] = {
+      id: canvas.id,
+      name: canvas.name,
+      vectorMap: {},
+      vectorList: []
+    }
+  }
+
+  const { vectorMap, vectorList } = canvasMap[canvas.id]
+
+  if (!vectorMap[node.id]) {
+    vectorMap[node.id] = node
+    vectorList.push(node.id)
+  }
+}
+
+function preprocessCanvas (canvas) {
+  canvas.children.forEach(child => {
+    if (child.visible !== false) {
+      preprocessTree(canvas, child)
+    }
+  })
+}
+
+function selectElement (name) {
+  const [element] = Object.keys(canvasMap).filter(current => canvasMap[current].name === name)
+  return element ? canvasMap[element] : element
+}
+
+function processColors () {
+  const colors = selectElement('Colors')
+
+  return colors.vectorList.map(v => {
+    const node = colors.vectorMap[v]
+    return node.children.map(color => {
+      const [swatch] = color.fills
+      return {
+        name: color.name,
+        background: figma.colorString(swatch.color, swatch.opacity)
+      }
+    })
+  })
+}
+
+async function figmaFileFetch () {
+  const result = await fetch(`https://api.figma.com/v1/files/${process.env.FILEID}`, {
+    method: 'GET',
+    headers: {
+      'X-Figma-Token': process.env.APIKEY
+    }
+  })
+
+  const data = await result.json()
+  const doc = data.document
+
+  doc.children.forEach(canvas => canvas.visible !== false && preprocessCanvas(canvas))
+
+  return processColors()
+}
+
+app.use('/', async function (req, res, next) {
+  const result = await figmaFileFetch().catch(error => console.log(error))
+  res.send(JSON.stringify(result))
+})
+
+const APP = {
+  port: 3000
+}
+
+app.listen(APP.port, () => console.log(`Figma api app listening at http://localhost:${APP.port}`))
